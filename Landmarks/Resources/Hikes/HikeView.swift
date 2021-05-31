8221fbcd55b6dc6d666ba0dd5b24d2f0a156c4fb
Dario Ascione <dario.ascione@gmail.com>
2021-05-31 16:13:05 +0200

    End animations and transition

--- Landmarks/Resources/Hikes/HikeView.swift
+++ Landmarks/Resources/Hikes/HikeView.swift
@@ -1,33 +1,51 @@
 /*
-See LICENSE folder for this sample’s licensing information.
-
-Abstract:
-A view displaying information about a hike, including an elevation graph.
-*/
+ See LICENSE folder for this sample’s licensing information.
+ 
+ Abstract:
+ A view displaying information about a hike, including an elevation graph.
+ */
 
 import SwiftUI
+
+extension AnyTransition {
+    
+    static var moveAndFade: AnyTransition {
+        
+        let insertion = AnyTransition.move(edge: .trailing)
+            .combined(with: .opacity)
+        
+        let removal = AnyTransition.scale
+            .combined(with: .opacity)
+        
+        return .asymmetric(insertion: insertion, removal: removal)
+        
+    }
+    
+}
 
 struct HikeView: View {
     var hike: Hike
     @State private var showDetail = false
-
+    
     var body: some View {
         VStack {
             HStack {
                 HikeGraph(hike: hike, path: \.elevation)
                     .frame(width: 50, height: 30)
                     .animation(nil)
-
+                
                 VStack(alignment: .leading) {
                     Text(hike.name)
                         .font(.headline)
                     Text(hike.distanceText)
                 }
-
+                
                 Spacer()
-
+                
                 Button(action: {
-                    self.showDetail.toggle()
+                    withAnimation{
+                        self.showDetail.toggle()
+                    }
                 }) {
                     Image(systemName: "chevron.right.circle")
                         .imageScale(.large)
@@ -36,9 +54,10 @@ struct HikeView: View {
                         .padding()
                 }
             }
-
+            
             if showDetail {
                 HikeDetail(hike: hike)
+                    .transition(.moveAndFade)
             }
         }
     }
