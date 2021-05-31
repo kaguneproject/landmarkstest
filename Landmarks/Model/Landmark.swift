f54e283105f9651afade2e2eb170d6af116713be
Dario Ascione <dario.ascione@gmail.com>
2021-05-31 16:34:48 +0200

    Categories

--- Landmarks/Model/Landmark.swift
+++ Landmarks/Model/Landmark.swift
@@ -10,34 +10,42 @@ import SwiftUI
 import CoreLocation
 
 struct Landmark: Hashable, Codable, Identifiable{
-
+    
     var id: Int
-
+    
     var name: String
-
+    
     var park: String
-
+    
     var state: String
-
+    
     var description: String
     
     var isFavorite: Bool
     
     private var imageName: String
-
+    
+    var category: Category
+    
+    enum Category: String, CaseIterable, Codable {
+        case lakes = "Lakes"
+        case rivers = "Rivers"
+        case mountains = "Mountains"
+    }
+    
     var image: Image {
         Image(imageName)
     }
     
     private var coordinates: Coordinates
-
+    
     struct Coordinates: Hashable, Codable {
         var latitude: Double
         var longitude: Double
     }
-
+    
     var locationCoordinate: CLLocationCoordinate2D {
         CLLocationCoordinate2D(latitude: coordinates.latitude,longitude: coordinates.longitude)
     }
-
+    
 }
---
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,30 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Dario Ascione on 31/05/21.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    @EnvironmentObject var modelData: ModelData
+    
+    var body: some View {
+        NavigationView {
+            List{
+                ForEach(modelData.categories.keys.sorted(), id: \.self){
+                    key in Text(key)
+                }
+            }
+            .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+            .environmentObject(ModelData())
+    }
+}
