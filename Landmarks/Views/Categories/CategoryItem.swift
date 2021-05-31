2db086d6b9310491a33ac6f91e5300431a64c87d
Dario Ascione <dario.ascione@gmail.com>
2021-05-31 17:19:40 +0200

    Category row

---
+++ Landmarks/Views/Categories/CategoryItem.swift
@@ -0,0 +1,32 @@
+//
+//  CategoryItem.swift
+//  Landmarks
+//
+//  Created by Dario Ascione on 31/05/21.
+//
+
+import SwiftUI
+
+struct CategoryItem: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/){
+            landmark
+                .image
+                .resizable()
+                .frame(width: 155, height: 155, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .cornerRadius(5)
+            
+            Text(landmark.name)
+                .font(.caption)
+        }
+        .padding(.leading,15)
+    }
+}
+
+struct CategoryItem_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryItem(landmark: ModelData().landmarks[0])
+    }
+}
